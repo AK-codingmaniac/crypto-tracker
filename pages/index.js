09a664b3d8984b8c807bbf1263081008dca8c237
Anshaj Khare <anshaj13.ugcs20@iiitranchi.ac.in>
2022-06-13 20:10:12 +0530

    Added Coin Data and Design

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,33 @@
-import Head from 'next/head'
+import { useState } from 'react';
 import Coinlist from '../components/Coinlist';
-import Coins from '../components/Coins'
 import Searchbar from '../components/Searchbar'
+import Layout from '../components/Layout';
 
 export default function Home({filteredCoins}) {
+
+  const [search,setSearch] = useState('');
+
+  const allCoins = filteredCoins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+    )
+
+  const handleChange = e =>{
+    e.preventDefault();
+    setSearch(e.target.value.toLowerCase());
+  }
+
   return (
-    <div>
-      <Head>
-        <title>Crypto-Tracker</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Searchbar type='text' placeholder='Search'/>
-      <Coinlist filteredCoins={filteredCoins}/>
-    </div>
+    <Layout>
+      <div className='coin_app'>
+      <Searchbar type='text' placeholder='Search' onChange={handleChange}/>
+      <Coinlist filteredCoins={allCoins}/>
+      </div>
+    </Layout>
   )
 }
 
 export const getServerSideProps = async ()=>{
-  const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+  const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false');
 
   const filteredCoins = await res.json();
 
